600dbb26259a599d67ea67134ab56d3ccd5bf4a1
Kendrick Shaw <kms15@case.edu>
2022-01-22 15:13:40 +0000

    state includes a sha256 of file contents
    Co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    Co-authored-by: Eric Herman <eric@freesa.org>

--- meshfssync/__init__.py
+++ meshfssync/__init__.py
@@ -1,7 +1,19 @@
 from pathlib import Path
 
+import hashlib
+
+def sha256_for_path(fp, blocksize = (4 * 1024 * 1024)):
+    hasher = hashlib.sha256()
+    buf = fp.read(blocksize)
+    # while len(buf) > 0:
+    assert len(buf) < blocksize
+    hasher.update(buf)
+    #    buf = fp.read(blocksize)
+    return hasher.hexdigest()
+
 def get_dir_state(dirpath):
     ds = {}
-    for x in dirpath.iterdir():
-        ds[x.name] = {}
+    for path in dirpath.iterdir():
+        with open(path, 'rb') as fp:
+            ds[path.name] = { 'sha256': sha256_for_path(fp) }
     return ds
--- test/test_init.py
+++ test/test_init.py
@@ -2,7 +2,14 @@ import meshfssync
 import tempfile
 from pathlib import Path
 
+
+
+
 def test_get_dir_state():
+    # $ echo 'foo' > foo.txt
+    # $ sha256sum foo.txt
+    # b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c foo.txt
+    foohash = 'b5bb9d8014a0f9b1d61e21e796d78dccdf1352f23cd32812f4850b878ae4944c'
     with tempfile.TemporaryDirectory() as tmpdirname:
         tmpdir = Path(tmpdirname)
         file1 = tmpdir.joinpath('file1.txt')
@@ -10,8 +17,10 @@ def test_get_dir_state():
         file1.write_text('foo\n')
         file2.write_text('bar\n')
 
+
         ds = meshfssync.get_dir_state(tmpdir);
         assert len(ds) == 2
         assert 'file1.txt' in ds
         assert 'file2.txt' in ds
 
+        assert ds['file1.txt']['sha256'] == foohash

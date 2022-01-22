0c186d5c84a11dd13bb82b4b3195eb44a7715154
Eric Herman <eric@freesa.org>
2022-01-22 14:40:49 +0000

    first non-dummy test
    Co-authored-by: Ace Medlock <ace.medlock@gmail.com>
    Co-authored-by: Kendrick Shaw <kms15@case.edu>

--- meshfssync/__init__.py
+++ meshfssync/__init__.py
@@ -1,2 +1,7 @@
-def dummy():
-    return True
+from pathlib import Path
+
+def get_dir_state(dirpath):
+    ds = {}
+    for x in dirpath.iterdir():
+        ds[x.name] = {}
+    return ds
--- test/test_init.py
+++ test/test_init.py
@@ -1,4 +1,17 @@
 import meshfssync
+import tempfile
+from pathlib import Path
 
-def test_dummy():
-    assert meshfssync.dummy()
+def test_get_dir_state():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        tmpdir = Path(tmpdirname)
+        file1 = tmpdir.joinpath('file1.txt')
+        file2 = tmpdir.joinpath('file2.txt')
+        file1.write_text('foo\n')
+        file2.write_text('bar\n')
+
+        ds = meshfssync.get_dir_state(tmpdir);
+        assert len(ds) == 2
+        assert 'file1.txt' in ds
+        assert 'file2.txt' in ds
+
